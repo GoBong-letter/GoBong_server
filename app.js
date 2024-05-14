d7e6fd9fc4c385cec2c190efa49f21ac8dccb2db
seohyunCho <shanny06@naver.com>
2024-05-15 01:47:01 +0900

    Feat: letters 라우터 추가

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(express.json());
 const users = require('./routes/user');
 app.use('/users', users);
 
+const letters = require('./routes/letter');
+app.use('/letters', letters);
+
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
