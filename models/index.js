82e73823a17e02ba8b2aeb0c79a9372e7be9f8f7
seohyunCho <shanny06@naver.com>
2024-05-15 01:45:51 +0900

    Feat: 날짜 설정

--- models/index.js
+++ models/index.js
@@ -3,23 +3,39 @@ const User = require('./users');
 const Card = require('./cards');
 const Category = require('./category');
 const UserCtg = require('./user_ctg')
+const Letter = require('./letters');
+const LetterCtg = require('./letter_ctg');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: "mysql",
+    timezone: "Asia/Seoul",
+    dialectOptions: {
+        charset: "utf8mb4",
+        dateStrings: true,
+        typeCast: true,
+    },
+});
+
 db.sequelize = sequelize;
 
 db.User = User;
 db.Card = Card;
 db.Category = Category;
 db.UserCtg = UserCtg;
+db.Letter = Letter;
+db.LetterCtg = LetterCtg
 
 User.init(sequelize);
 Card.init(sequelize);
 Category.init(sequelize);
 UserCtg.init(sequelize);
+Letter.init(sequelize);
+LetterCtg.init(sequelize);
 
 db.User.belongsToMany(db.Category, {
     through: 'UserCtg',
@@ -29,6 +45,18 @@ db.User.belongsToMany(db.Category, {
 
 db.Category.belongsToMany(db.User, {
     through: 'UserCtg',
+    foreignKey: "ctg_id",
+    sourceKey: "id",
+});
+
+db.Letter.belongsToMany(db.Category, {
+    through: 'LetterCtg',
+    foreignKey: "letter_id",
+    sourceKey: "id",
+});
+
+db.Category.belongsToMany(db.Letter, {
+    through: 'LetterCtg',
     foreignKey: "ctg_id",
     sourceKey: "id",
 });
