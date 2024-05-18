c01262dd5b126d39499d513fab264b4480feb82f
seohyunCho <shanny06@naver.com>
2024-05-19 01:17:11 +0900

    Feat: 사용자 정보 조회, 카테고리 수정 api 구현

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,9 @@ const usersMiddleware = require('../controllers/user');
 
 router.post('/signup', usersMiddleware.signupPostMid);
 router.post('/login', usersMiddleware.loginPostMid);
+router.get('/:user_id', usersMiddleware.userInfoGetMind);
 router.patch('/image', usersMiddleware.imagePatchMid)
 router.patch('/nickname', usersMiddleware.nicknamePatchMid)
+router.patch('/category', usersMiddleware.categoryPatchMid);
 
 module.exports = router;
