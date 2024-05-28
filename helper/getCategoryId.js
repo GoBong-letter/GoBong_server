11e245c55ae5bdc502c97a8ceecbbdefc27df72e
seohyunCho <shanny06@naver.com>
2024-05-29 03:25:45 +0900

    Fix: 카테고리 저장 방법 수정

--- helper/getCategoryId.js
+++ helper/getCategoryId.js
@@ -1,19 +1,38 @@
-const { UserCtg, LetterCtg, Category } = require('../models');
+const { UserCtg, LetterCtg, Category, SubCategory } = require('../models');
 
 async function getCategoryId(b_ctg, name, id, id_name, table) {
     try {
-      const categoryId = await Category.findAll({
-        attributes: ['id'], // 선택할 속성 지정
-        where: {
-          b_ctg: b_ctg,
-          name: name
+      const category = await Category.findOne({
+        attributes: ['id'],
+        where:{
+          name: b_ctg
         }
-      });
-  
-      await table.create({
-        [id_name]: id,
-        ctg_id: categoryId[0].dataValues.id
       })
+
+      const categoryName = category.dataValues.id
+
+      if(categoryName === 6) { // 기타일 때
+        await table.create({
+          [id_name]: id,
+          category_id: categoryName,
+          value: name
+        })
+      }else{ // 정해진 값일 때
+        const subCategory = await SubCategory.findOne({
+          attributes: ['id'],
+          where:{
+            category_id: categoryName,
+            name: name
+          }
+        })
+        const subCategoryId = subCategory.dataValues.id;
+
+        await table.create({
+          [id_name]: id,
+          category_id: categoryName,
+          subcategory_id: subCategoryId
+        })
+      }
 
     } catch (error) {
       console.error('Error:', error);
