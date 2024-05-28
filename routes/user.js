629dd46820998bea45a8d00230d7831be10472c5
seohyunCho <shanny06@naver.com>
2024-05-29 03:26:15 +0900

    Comment: 라우터 주석 추가

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const usersMiddleware = require('../controllers/user');
 
-router.post('/signup', usersMiddleware.signupPostMid);
-router.post('/login', usersMiddleware.loginPostMid);
-router.get('/:user_id', usersMiddleware.userInfoGetMind);
-router.get('/card/:user_id', usersMiddleware.cardsGetMid);
-router.patch('/image', usersMiddleware.imagePatchMid)
-router.patch('/nickname', usersMiddleware.nicknamePatchMid)
-router.patch('/category', usersMiddleware.categoryPatchMid);
+router.post('/signup', usersMiddleware.signupPostMid); // 회원가입
+router.post('/login', usersMiddleware.loginPostMid); // 로그인
+router.get('/:user_id', usersMiddleware.userInfoGetMind); // 유저 정보 조회
+router.get('/card/:user_id', usersMiddleware.cardsGetMid); // 유저의 카드 정보 조회
+router.patch('/image', usersMiddleware.imagePatchMid); // 이미지 수정
+router.patch('/nickname', usersMiddleware.nicknamePatchMid) // 닉네임 수정
+router.patch('/category', usersMiddleware.categoryPatchMid); // 카테고리 수정
 
 module.exports = router;
